bc6a2bb2f06bd337cdd74dfd703ffef78bf27ac1
vladislavsumin <vladislavsumin@mail.ru>
2017-04-25 21:51:01 +0300

    Finish version

--- src/ru/suminvladislav/Main.java
+++ src/ru/suminvladislav/Main.java
@@ -1,25 +1,16 @@
 package ru.suminvladislav;
 
-import java.util.Vector;
-
 public class Main {
     private static long time;
 
     public static void main(String[] args) {
-        Vector<Long> times = new Vector<>();
-        for (int i = 0; i < 100; i++) {
-            time = System.currentTimeMillis();
-            long n = (long) Integer.MAX_VALUE - 1;
-            //long n = 987654321L;
-            //long n = Long.valueOf(args[0]);
-            System.out.println("N: " + n);
-            new PrimeNumber(n);
-            time = System.currentTimeMillis() - time;
-            System.out.println("Time: " + time + " ms");
-            times.add(time);
-            time = 0;
-            times.forEach(aLong -> time += aLong);
-            System.out.println("Avg time: " + (time / ((double) times.size())) + " ms\n");
-        }
+        time = System.currentTimeMillis();
+        //long n = (long) Integer.MAX_VALUE - 1;
+        //long n = 987654321L;
+        long n = Long.valueOf(args[0]);
+        System.out.println("N: " + n);
+        new PrimeNumber(n);
+        time = System.currentTimeMillis() - time;
+        System.out.println("Time: " + time + " ms");
     }
 }
--- src/ru/suminvladislav/PrimeNumber.java
+++ src/ru/suminvladislav/PrimeNumber.java
@@ -1,22 +1,21 @@
 package ru.suminvladislav;
 
-
 class PrimeNumber {
-    private final long max; // Кол-во чисел после оптимизации.
-    private long currentNumber = 1;
+    private final long max; // Кол-во чисел после оптимизации
+    private long currentNumber = 1; // Кол-во найденных чисел
     private final int sizeI; // Размер массива после деления на SizeJ
-    private static final long sizeJ = 64; // Размер лонга
+    private static final int sizeJ = 64; // Размер лонга в битах
     private final long[] vector;
 
     PrimeNumber(long max) {
-        this.max = ((long) Math.ceil(max / 2.0)) + 1; // тут тип в 2 раза меньше чисел.
+        this.max = ((long) Math.ceil(max / 2.0)) + 1; // Двойная экономия памяти
         sizeI = (int) (this.max / sizeJ + 1);
         vector = new long[sizeI];
         process();
         System.out.println("Total: " + currentNumber);
 
         //Поиск последнего найденного.
-        long lastIndex = this.max;
+        long lastIndex = this.max - 1;
         while (((vector[(int) (lastIndex / sizeJ)] >> (int) (lastIndex % sizeJ)) & 1) == 1) lastIndex--;
         System.out.println("Last number: " + (lastIndex * 2 - 1));
     }
@@ -26,12 +25,12 @@ class PrimeNumber {
         long step, index;
         //System.out.println(2);
         for (; lastIndex < max; lastIndex++) {
-            if (((vector[(int) (lastIndex / sizeJ)] >> ((int) (lastIndex % sizeJ))) & 1) == 1) continue;
+            if (((vector[(int) (lastIndex / sizeJ)] >> (lastIndex % sizeJ)) & 1) == 1) continue;
             step = lastIndex * 2 - 1;
             index = lastIndex + (lastIndex - 1) * step; // Вычеркивание начиная с n^2
             if (index > max) break;
             while (index < max) {
-                vector[(int) (index / sizeJ)] |= (1L << (int) (index % sizeJ));
+                vector[(int) (index / sizeJ)] |= (1L << (index % sizeJ));
                 index += step;
             }
             //System.out.println(lastIndex * 2 - 1);
@@ -39,7 +38,7 @@ class PrimeNumber {
         }
 
         for (; lastIndex < max; lastIndex++) {
-            while (((vector[(int) (lastIndex / sizeJ)] >> (int) (lastIndex % sizeJ)) & 1) == 1)
+            while (((vector[(int) (lastIndex / sizeJ)] >> (lastIndex % sizeJ)) & 1) == 1)
                 lastIndex++;
             if (lastIndex < max) {
                 currentNumber++;
