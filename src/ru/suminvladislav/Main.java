c07df832162ba9c7d2add9516de110df88728c5b
vladislavsumin <vladislavsumin@mail.ru>
2017-04-26 21:18:54 +0300

    Version 1.1-RELEASE

--- src/ru/suminvladislav/Main.java
+++ src/ru/suminvladislav/Main.java
@@ -1,16 +1,10 @@
 package ru.suminvladislav;
 
 public class Main {
-    private static long time;
-
     public static void main(String[] args) {
-        time = System.currentTimeMillis();
-        //long n = (long) Integer.MAX_VALUE - 1;
+        long n = (long) Integer.MAX_VALUE - 1;
         //long n = 987654321L;
-        long n = Long.valueOf(args[0]);
-        System.out.println("N: " + n);
+        //long n = Long.valueOf(args[0]);
         new PrimeNumber(n);
-        time = System.currentTimeMillis() - time;
-        System.out.println("Time: " + time + " ms");
     }
 }
--- src/ru/suminvladislav/PrimeNumber.java
+++ src/ru/suminvladislav/PrimeNumber.java
@@ -1,5 +1,11 @@
 package ru.suminvladislav;
 
+/**
+ * Вычисление простых чисел с помощью решета Эратосфена
+ *
+ * @author Sumin Vladislav
+ * @version 1.1
+ */
 class PrimeNumber {
     private final long max; // Кол-во чисел после оптимизации
     private long currentNumber = 1; // Кол-во найденных чисел
@@ -7,22 +13,39 @@ class PrimeNumber {
     private static final int sizeJ = 64; // Размер лонга в битах
     private final long[] vector;
 
+    /**
+     * @param max - верхний предел поиска
+     */
     PrimeNumber(long max) {
+        long time = System.currentTimeMillis();
+        System.out.println("N: " + max);
+
         this.max = ((long) Math.ceil(max / 2.0)) + 1; // Двойная экономия памяти
         sizeI = (int) (this.max / sizeJ + 1);
         vector = new long[sizeI];
+
         process();
+
         System.out.println("Total: " + currentNumber);
 
         //Поиск последнего найденного.
         long lastIndex = this.max - 1;
         while (((vector[(int) (lastIndex / sizeJ)] >> (int) (lastIndex % sizeJ)) & 1) == 1) lastIndex--;
         System.out.println("Last number: " + (lastIndex * 2 - 1));
+
+        // Вывод информации о затраченном времени
+        time = System.currentTimeMillis() - time;
+        System.out.println("Time: " + time + " ms");
     }
 
+    /**
+     * Внимание: вывод всех чисел в консоль занимает в 35 раз больше времени чем их поиск (при max = 2^31 - 2).
+     *
+     */
     private void process() {
         long lastIndex = 2;
         long step, index;
+        // Вывод самих чисел
         //System.out.println(2);
         for (; lastIndex < max; lastIndex++) {
             if (((vector[(int) (lastIndex / sizeJ)] >> (lastIndex % sizeJ)) & 1) == 1) continue;
@@ -33,6 +56,7 @@ class PrimeNumber {
                 vector[(int) (index / sizeJ)] |= (1L << (index % sizeJ));
                 index += step;
             }
+            // Вывод самих чисел
             //System.out.println(lastIndex * 2 - 1);
             currentNumber++;
         }
@@ -41,6 +65,8 @@ class PrimeNumber {
             while (((vector[(int) (lastIndex / sizeJ)] >> (lastIndex % sizeJ)) & 1) == 1)
                 lastIndex++;
             if (lastIndex < max) {
+                // Вывод самих чисел
+                //System.out.println(lastIndex * 2 - 1);
                 currentNumber++;
             }
         }
